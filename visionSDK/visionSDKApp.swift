b8247d959eb2008052a6c49be0c038f037d5f158
Fernando Salom <fer@rudo.es>
2024-03-24 15:44:18 +0100

    Add initial project

---
+++ visionSDK/visionSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionSDKApp.swift
+//  visionSDK
+//
+//  Created by Fernando Salom Carratala on 24/3/24.
+//
+
+import SwiftUI
+
+@main
+struct visionSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
